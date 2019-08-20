bec07292540a1bb7a0463bf8aac458f49ed0e42a
Ryan McIntyre <ryankmcintyre@gmail.com>
2019-08-20 15:04:57 -0600

    Update healthroute_test.go

--- apis/trips/tests/healthroute_test.go
+++ apis/trips/tests/healthroute_test.go
@@ -13,8 +13,8 @@ var healthRouteTests = []tripSvc.APITestCase{
 		Tag:              "t0 - healthcheck",
 		Method:           "GET",
 		URL:              "/api/healthcheck/trips",
-		Status:           200,
-		ExpectedResponse: `{"message": "Trip Service Healthcheck","status": "HealthyFAIL"}`,
+		Status:           400,
+		ExpectedResponse: `{"message": "Trip Service Healthcheck","status": "Healthy"}`,
 	},
 }
 

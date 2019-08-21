d1cb97a0a6b637414a1f0728fcf03b241c27e84c
Ryan McIntyre <ryankmcintyre@gmail.com>
2019-08-21 09:22:35 -0600

    Update HealthCheckTests.cs

--- apis/poi/tests/UnitTests/HealthCheckTests.cs
+++ apis/poi/tests/UnitTests/HealthCheckTests.cs
@@ -10,7 +10,7 @@ namespace UnitTests
         public void HealthCheckTestModel()
         {
             Assert.Equal("POI Service Healthcheck", new Healthcheck().Message);
-            Assert.Equal("Healthy as Fuck", new Healthcheck().Status);
+            Assert.Equal("Healthy as fuck", new Healthcheck().Status);
 
         }
     }
